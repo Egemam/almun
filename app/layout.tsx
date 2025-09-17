f21a43353e6f269e751860b569ebea28147af3cf
Egemam <destekbekleniyor009@gmail.com>
2025-09-17 09:25:48 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import NavBar from "@/app/ui/navbar";
 import "./globals.css";
+import Link from "next/link";
 
 const poppins = Poppins({
     variable: "--font-poppins",
@@ -13,6 +14,15 @@ export const metadata: Metadata = {
   title: "ALMUN'25",
   description: "Antalya Lisesi Model United Nations Conference 2025",
 };
+
+function Footer(){
+    return(
+        <footer className="w-full h-10 flex justify-center items-center">
+            <Link href={"/"}>Instagram</Link>
+            <Link href="https://wa.me/905414408739" className="underline"></Link>
+        </footer>
+    )
+}
 
 // #58120f r
 // #09090b b
@@ -30,6 +40,7 @@ export default function RootLayout({
       >
         <NavBar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
